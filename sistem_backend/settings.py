c1a43f779c99364f5b105c49f94a17db738d5bb9
Kyouji Prinz <udiadhahaskaananta@gmail.com>
2026-06-08 19:12:28 +0700

    Fix: Tambahkan domain Render ke ALLOWED_HOSTS

--- sistem_backend/settings.py
+++ sistem_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(kcjkiz3p5ob%p=j93b8ae*vgki_bzd2kq37%t+8jz)vkyz)%b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
