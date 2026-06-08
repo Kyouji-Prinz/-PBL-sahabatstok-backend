5316aa248df1a82ae3d59a339c8a8b8a832b881a
Kyouji Prinz <udiadhahaskaananta@gmail.com>
2026-06-08 19:52:37 +0700

    Fix: urls py sistem backend pt. 3

--- sistem_backend/urls.py
+++ sistem_backend/urls.py
@@ -1,25 +1,13 @@
-"""
-URL configuration for sistem_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.views.generic import RedirectView
 from django.contrib import admin
 from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # 1. Trik Redirect: Jika dosen membuka link onrender.com, otomatis dilempar ke UI web GitHub
+    path('', RedirectView.as_view(url='https://kyouji-prinz.github.io/-PBL-sahabatstok-backend/', permanent=False)),
+    
+    # 2. Rute API dibiarkan lewat agar frontend bisa menembak endpoint /api/predict/ dsb.
     path('', include('api_stok.urls')),
-    path('', RedirectView.as_view(url='https://github.com/Kyouji-Prinz/-PBL-sahabatstok-backend')),
 ]
